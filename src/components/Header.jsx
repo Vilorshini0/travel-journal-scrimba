e6ff4c4a73ecd7a14006852b05a0c48b8c2370a5
Vilorshini <ramsamyvilorshini@gmail.com>
2025-09-30 12:16:00 +0400

    [INTE] header integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import globe from '../assets/Globe.png'
 export default function Header() {
     return (
         <header>
-            <img src={globe} alt="" />
+            <img src={globe} alt="Globe icon" />
             <h1>my travel journal</h1>
         </header>
     )
