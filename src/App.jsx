9aa003607db5c8787580da28221b44597f098d1a
Vilorshini <ramsamyvilorshini@gmail.com>
2025-10-02 23:48:52 +0400

    [INTE] Added entry component and data.js file for the entry list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Header from './components/Header'
+import Entry from './components/Entry'
 
 function App() {
     return (
         <>
             <Header/>
+            <main className='journalEntry'>
+                <Entry/>
+                <div id='testing'></div>
+            </main>
         </>
     )
 }
